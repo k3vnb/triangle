a75b0a2eb0a8c0a00525640c112e32d746fe2783
Adam Smith and Kevin Boyle <adam@>
2017-10-19 15:34:12 -0700

    create file, link css & js, add forms, button, add jQuery to pull value from form & manipulate

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  $("form#triangleform").submit(function(event) {
+    var sideA = parseFloat($("input#sidea").val());
+    var sideB = parseFloat($("input#sideb").val());
+    var sideC = parseFloat($("input#sidec").val());
+
+    if (isNaN(sideA) || isNaN(sideB) || isNaN(sideC)) {
+      alert("Please enter actualy numbers into the form this time.");
+    } else {
+      if (sideA === sideB && sideA === sideC) {
+        alert("Whoo boy! You've got an equileteral triangle on your hands!");
+      } else if (sideA + sideB <= sideC || sideB + sideC <= sideA || sideA + sideC <= sideB) {
+        alert("That, my friend, is no triangle! That is a lie!");
+      } else {
+        alert("end");
+      };
+
+
+    };
+    event.preventDefault();
+  });
+});
+// parseFloat($("#add2").val());
